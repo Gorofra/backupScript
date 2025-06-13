f5e3493399aab007fb66a0f622fa4178d58117ef
Gorofra <francescogorini01@gmail.com>
2025-06-13 16:00:32 +0200

    test ok backup sql creato correttamente in onedrive windows

--- backupScript.py
+++ backupScript.py
@@ -8,16 +8,57 @@ from pathlib import Path
 from dotenv import load_dotenv
 
 
+##################################
+#       Metodi e funzioni        #
+##################################
 
- # subprocess.run(["docker","exec","--help"])
 
+def checkBackupFolder():
+    backupPath = Path(onedrivePath) / "Backup"
+    if not backupPath.exists():
+        print("Creazione cartella di backup...")
+        backupPath.mkdir(parents=True, exist_ok=True)
+        print("Cartella di backup creata.")
+        
+    else:
+        print("Cartella backup trovata.")
+        
+    return backupPath
+
+def backupMysql():
+
+    backupPath = checkBackupFolder()
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+    backupFile = backupPath / f"backup_{timestamp}.sql"
+
+    db_container_name = os.getenv('DB_CONTAINER_NAME')
+    db_user = os.getenv('DB_USER')
+    db_password = os.getenv('DB_PASSWORD')
+    db_name = os.getenv('DB_NAME')
+
+    try:
+        print("Eseguendo il backup del database...")
+        subprocess.run([
+            "docker", "exec", db_container_name,
+            "mysqldump", "-u", db_user, 
+            f"-p{db_password}", db_name
+        ], stdout=open(backupFile, 'w'), check=True)
+        print(f"Backup completato: {backupFile}")
+    except subprocess.CalledProcessError as e:
+        print(f"Errore durante il backup: {e}")
+        sys.exit(1)
+    
 ##################################
 #      Caricamento variabili     #
 ##################################
 
+
+# subprocess.run(["docker","exec","--help"])
+
+
 load_dotenv()
 
-required_vars = [
+requiredVars = [
     "DB_CONTAINER_NAME",
     "DB_USER",
     "DB_PASSWORD",
@@ -25,21 +66,31 @@ required_vars = [
     "USERNAME"
 ]
 
-missing_vars=[var for var in required_vars if not os.getenv(var)]
+missingVars=[var for var in requiredVars if not os.getenv(var)]
 
-if missing_vars:
-    print(f"ERRORE: Variabili mancanti nel .env: {', '.join(missing_vars)}")
+if missingVars:
+    print(f"ERRORE: Variabili mancanti nel .env: {', '.join(missingVars)}")
     exit(1)
 
 print (" variabili d'ambiente caricate correttamente")
 
 
 #####################################
-# Creazione cartella se inesistente #
+#       Controllo directory         #
 #####################################
+
+
 username = os.getenv('USERNAME')
 onedrivePath = f"C:\\Users\\{username}\\OneDrive"
 if os.path.exists(onedrivePath):
     print("cartella onedrive trovata")
+    checkBackupFolder()
 else:
     print("cartella onedrive NON trovata")
+    
+    
+#####################################
+#            START BACKUP           #
+#####################################
+
+backupMysql()
