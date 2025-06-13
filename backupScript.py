2c0542073df51cd5aef604456f6bb49817187a8c
Gorofra <francescogorini01@gmail.com>
2025-06-13 12:14:00 +0200

    FIrst commit backupManager Windows

---
+++ backupScript.py
@@ -0,0 +1,33 @@
+import os
+import sys
+import docker
+import subprocess
+import logging
+from datetime import datetime
+from pathlib import Path
+from dotenv import load_dotenv
+
+
+
+ # subprocess.run(["docker","exec","--help"])
+
+
+
+load_dotenv()
+
+required_vars = [
+    "DB_CONTAINER_NAME",
+    "DB_USER",
+    "DB_PASSWORD",
+    "DB_NAME",
+]
+
+missing_vars=[var for var in required_vars if not os.getenv(var)]
+
+if missing_vars:
+    print(f"ERRORE: Variabili mancanti nel .env: {', '.join(missing_vars)}")
+    exit(1)
+
+print (" variabili d'ambiente caricate correttamente")
+
+
